1d3576fda89dc8f401f4913224315cfafcfd2151
Timen Olthof <timen@timenolthof.nl>
2017-10-11 15:21:25 +0200

    feat: requireAttribute middleware

--- src/diva.js
+++ src/diva.js
@@ -18,5 +18,20 @@ exports.deauthenticate = function deauthenticate() {
       sessionId: uuidv4(),
       attributes: [],
     },
-  };;
+  };
+}
+
+exports.requireAttribute = function requireAttribute(attribute) {
+  return function (req, res, next) {
+    if (req.divaSessionState.user.attributes.indexOf(attribute) > -1) {
+      next();
+    } else {
+      res
+        .status(401)
+        .send({
+          success: false,
+          message: `You are missing attribute  ${attribute}`,
+        });
+    }
+  };
 }
