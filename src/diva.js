80f7100b64db1ef938b894a1c063ff6e6f3f2524
Timen Olthof <timen@timenolthof.nl>
2017-10-11 10:43:59 +0200

    feat: add simple addProof method stub

--- src/diva.js
+++ src/diva.js
@@ -3,3 +3,7 @@ const packageJson = require('./../package.json');
 exports.version = function version() {
   return packageJson.version;
 };
+
+exports.addProof = function addProof(divaSessionState, proof) {
+  divaSessionState.user.attributes.push(proof);
+}
