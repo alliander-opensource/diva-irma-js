52aef7fc8bbcd1f5f638f77f118cddf60f111285
Timen Olthof <timen@timenolthof.nl>
2017-10-11 14:34:18 +0200

    feat: add deauthenticate functionality

--- src/diva.js
+++ src/diva.js
@@ -1,9 +1,22 @@
+const uuidv4 = require('uuid/v4');
+
 const packageJson = require('./../package.json');
 
 exports.version = function version() {
   return packageJson.version;
 };
 
+// TODO make this more functional
 exports.addProof = function addProof(divaSessionState, proof) {
   divaSessionState.user.attributes.push(proof);
+  return divaSessionState;
+}
+
+exports.deauthenticate = function deauthenticate() {
+  return {
+    user: {
+      sessionId: uuidv4(),
+      attributes: [],
+    },
+  };;
 }
